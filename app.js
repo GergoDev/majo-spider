f4267fe4ea95a034522595f3af8d5e8080cb45d5
Gergo <ferenczi.gergo.87@gmail.com>
2020-02-02 01:24:34 +0000

    Basic function of application

---
+++ app.js
@@ -0,0 +1,42 @@
+const axios = require('axios')
+const mongodb = require('mongodb')
+var schedule = require('node-schedule');
+
+const videoId = "DixKZZBsmso"
+const youtubeApiKey = "AIzaSyB5VDNjUt-kuuShxiYbnzuV2wsbmhEscu4"
+
+mongodb.connect("mongodb+srv://todoAppUser:todoappjelszo@cluster0-wtowc.mongodb.net/YouTube?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+
+  function sendDataToMongo(data) {
+    
+    client.db().collection("views").insertOne(data)
+      console.log("Insert done, Ready")
+
+  }
+
+  var j = schedule.scheduleJob('1 * * * *', function(){
+
+    axios.get('https://www.googleapis.com/youtube/v3/videos', {
+        params: {
+          part: "statistics",
+          id: videoId,
+          key: youtubeApiKey
+        }
+      })
+      .then(function (response) {
+        let dataToInsert = {
+          VideoID: videoId,
+          ViewCounter: response.data.items[0].statistics.viewCount,
+          Time: new Date()
+        }  
+        
+        sendDataToMongo(dataToInsert)
+                
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+
+  })
+
+})
