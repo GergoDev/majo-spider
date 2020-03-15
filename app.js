1b5ee07ff28f753e31e3ed015c51b6be1f2dea99
Gergo <ferenczi.gergo.87@gmail.com>
2020-03-15 00:32:39 +0000

    db in separate file

--- app.js
+++ app.js
@@ -1,53 +1,39 @@
 const axios = require('axios')
-const mongodb = require('mongodb')
+const viewsCollection = require('./db').db().collection('views')
 const schedule = require('node-schedule')
 const dotenv = require('dotenv')
 dotenv.config()
 
 const videoId = "DixKZZBsmso"
-const youtubeApiKey = "AIzaSyB5VDNjUt-kuuShxiYbnzuV2wsbmhEscu4"
 
 /* axios.get('https://www.googleapis.com/youtube/v3/videos', {
-        params: {
-          part: "statistics",
-          id: videoId,
-          key: youtubeApiKey
-        }
-      })
-      .then(function (response) {
-        let dataToInsert = {
-          VideoID: videoId,
-          ViewCounter: response.data.items[0].statistics.viewCount,
-          Time: new Date()
-        }  
-        
-        sendDataToMongo(dataToInsert)
-                
-      })
-      .catch(function (error) {
-        console.log(error);
-      }) */
-
-mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
-
-  function sendDataToMongo(data) {
+    params: {
+      part: "statistics",
+      id: videoId,
+      key: process.env.YOUTUBEAPIKEY
+    }
+}).then(function (response) {
+    let dataToInsert = {
+      VideoID: videoId,
+      ViewCounter: response.data.items[0].statistics.viewCount,
+      Time: new Date()
+    }  
     
-    client.db().collection("views").insertOne(data)
-    console.log("Insert done, Ready")
+    sendDataToMongo(dataToInsert)
+          
+}).catch(function (error) {
+    console.log(error);
+}) */
 
-  }
+    
+    viewsCollection.insertOne({
+      VideoID: videoId,
+      Time: new Date()
+    }, () => console.log("Insert done, Ready"))
 
-  let dataToInsert = {
-    VideoID: videoId,
-    Time: new Date()
-  }  
-  
-  sendDataToMongo(dataToInsert)
 
   // var j = schedule.scheduleJob('*/5 * * * * *', function(){
 
     
 
   // }) 
-
-})
